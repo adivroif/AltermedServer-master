2d4a0458fbede64f863f6c11731fbaf5ea60eb29
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-04-11 21:11:07 +0300

    Update Migrations with new ones

---
+++ Migrations/20250410151525_AddFeedbackEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltermedManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFeedbackEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PatientFeedbacks",
+                columns: table => new
+                {
+                    feedbackId = table.Column<Guid>(type: "uuid", nullable: false),
+                    patientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    appointmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    overallStatus = table.Column<int>(type: "integer", nullable: false),
+                    newSymptoms = table.Column<string>(type: "text", nullable: false),
+                    comments = table.Column<string>(type: "text", nullable: false),
+                    createdOn = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientFeedbacks", x => x.feedbackId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatientFeedbacks");
+        }
+    }
+}
---
+++ Migrations/20250410151821_AddFeedbackEntity-ApllDBContextUpdate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltermedManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFeedbackEntityApllDBContextUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PatientFeedbacks",
+                table: "PatientFeedbacks");
+
+            migrationBuilder.RenameTable(
+                name: "PatientFeedbacks",
+                newName: "PatientFeedbacks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PatientFeedbacks",
+                table: "PatientFeedbacks",
+                column: "feedbackId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PatientFeedbacks",
+                table: "PatientFeedbacks");
+
+            migrationBuilder.RenameTable(
+                name: "PatientFeedbacks",
+                newName: "PatientFeedbacks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PatientFeedbacks",
+                table: "PatientFeedbacks",
+                column: "feedbackId");
+        }
+    }
+}
