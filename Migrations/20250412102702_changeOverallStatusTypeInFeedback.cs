d7ef0553da397067827f5d7e908fbed4f5ad5ad1
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-04-12 13:30:27 +0300

    Change overallStatus type to float in PatientFeedback

---
+++ Migrations/20250412102702_changeOverallStatusTypeInFeedback.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltermedManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeOverallStatusTypeInFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AlterColumn<float>(
+                name: "overallStatus",
+                table: "PatientFeedbacks",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AlterColumn<int>(
+                name: "overallStatus",
+                table: "PatientFeedbacks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
