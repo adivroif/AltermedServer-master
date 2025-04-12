62bb609da263c6be700cbf90143edceda03ee5ed
adivr <adiv.roif@s.afeka.ac.l>
2025-04-12 14:28:36 +0300

    change overallStatus type in newFeedbackDto to float.

--- Models/Dtos/NewPatientFeedbackDto.cs
+++ Models/Dtos/NewPatientFeedbackDto.cs
@@ -5,7 +5,7 @@
              public Guid feedbackId { get; set; }
              public Guid patientId { get; set; }
              public Guid appointmentId { get; set; }
-             public int overallStatus { get; set; }
+             public float overallStatus { get; set; }
              public string newSymptoms { get; set; }
              public string comments { get; set; }
              public DateTime createdOn { get; set; }
