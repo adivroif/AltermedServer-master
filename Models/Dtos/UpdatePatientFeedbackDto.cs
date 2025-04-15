5a7bda5629e47a90db39ac78a932655a110bb7e7
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-04-15 12:18:21 +0300

    Merge from master: update patient requests service

--- Models/Dtos/UpdatePatientFeedbackDto.cs
+++ Models/Dtos/UpdatePatientFeedbackDto.cs
@@ -7,7 +7,7 @@ namespace AltermedManager.Models.Dtos
         public Guid feedbackId { get; set; }
         public Guid patientId { get; set; }
         public Guid appointmentId { get; set; }
-        public int overallStatus { get; set; }
+        public float overallStatus { get; set; }
         public string newSymptoms { get; set; }
         public string comments { get; set; }
         public DateTime createdOn { get; set; }
