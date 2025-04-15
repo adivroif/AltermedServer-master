a1f2825d21ab3db536c79be71b127a0d621be272
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-04-15 12:16:50 +0300

    Merge from master: update application db contet

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,8 +11,12 @@ namespace AltermedManager.Data
         }
 
         public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientRequest> PatientRequest { get; set; }
+
         public DbSet<Appointment> Appointments { get; set; }
         public DbSet<AppointmentSlots> AppointmentSlots { get; set; }
+
+        public DbSet<User> Users { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Treatment> Treatments { get; set; }
         public DbSet<PatientFeedback> PatientFeedbacks { get; set; }
