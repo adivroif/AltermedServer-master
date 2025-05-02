b428ff00a358b387ad80eee0938e2642a04cdc29
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-05-02 16:33:23 +0300

    Create AnalyzedFeedback for internal recommendation using

---
+++ Models/Dtos/AnalyzedFeedback.cs
@@ -0,0 +1,10 @@
+﻿namespace AltermedManager.Models.Dtos
+    {
+    public class AnalyzedFeedback
+        {
+        public int treatmentId { get; set; }
+        public double score { get; set; }
+        public string bodyPart { get; set; }
+        }
+
+    }
---
+++ Services/FeedbackAnalysisServer.cs
@@ -0,0 +1,49 @@
+﻿using AltermedManager.Data;
+using AltermedManager.Models.Dtos;
+using AltermedManager.Models.Entities;
+using AltermedManager.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace AltermedManager.Services
+    {
+    public class FeedbackAnalysisServer
+        {
+        private readonly ApplicationDbContext _context;
+
+
+        public FeedbackAnalysisServer(ApplicationDbContext context)
+            {
+            _context = context;
+            }
+
+        /*
+         * This function is based on users feedbacks.
+         * The goal is to find all positive feedbacks about treatments that treat
+         * a specific body part.         
+         */
+        public List<AnalyzedFeedback> GetHighFeedbacksByBodyPart(string bodyPart, Treatment badTreatment)
+            {
+
+            var normalizedParts = new List<string>();
+            if (bodyPart.Contains("יד"))
+                normalizedParts.AddRange(new[] { "יד שמאלית", "יד ימנית" });
+            else if (bodyPart.Contains("רגל"))
+                normalizedParts.AddRange(new[] { "רגל שמאלית", "רגל ימנית" });
+            else
+                normalizedParts.Add(bodyPart);
+
+            var feedbacks = (from feedback in _context.PatientFeedbacks
+                             where normalizedParts.Contains(feedback.bodyPart) 
+                                && feedback.overallStatus >= TreatmentsConst.TREATMENTS_MIN_SCORE
+                             join appointment in _context.Appointments
+                                on feedback.appointmentId equals appointment.appointmentId
+                             select new AnalyzedFeedback
+                                { treatmentId = appointment.treatmentId,
+                                score = feedback.overallStatus,
+                                bodyPart = feedback.bodyPart
+                                }).ToList();
+            return feedbacks;
+            }
+
+        }
+    }
