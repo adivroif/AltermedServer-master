5402f398cf9b5c0bdd461090e00ec083e549769b
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-05-02 08:53:45 +0300

    Separate feedbacks controller to controller and service

--- Controllers/PatientsFeedbacksController.cs
+++ Controllers/PatientsFeedbacksController.cs
@@ -10,114 +10,73 @@ using Microsoft.EntityFrameworkCore;
 using System.Xml.Linq;
 
 namespace AltermedManager.Controllers
-{
+    {
     [Route("api/[controller]")]
     [ApiController]
     public class PatientsFeedbacksController : ControllerBase
-    {
-        private readonly ApplicationDbContext dbContext;
-        private readonly TreatmentService _treatmentService;
-        private readonly RecommendationService _recommendationService;
-        public PatientsFeedbacksController(ApplicationDbContext dbContext, TreatmentService treatmentService,
-            RecommendationService recommendationService )
         {
-            this.dbContext = dbContext;
-            _treatmentService = treatmentService;
-            _recommendationService = recommendationService;
-
-        }
+        private readonly PatientsFeedbacksService _feedbacksService;
+        public PatientsFeedbacksController(PatientsFeedbacksService feedbacksService)
+            {
+            _feedbacksService = feedbacksService;
+            }
 
         [HttpGet]
         public IActionResult GetAllPatientsFeedbacks()
-        {
-            var allPatientsFeedbacks = dbContext.PatientFeedbacks.ToList();
-            return Ok(allPatientsFeedbacks);
-        }
-
-
-
-        [HttpGet("pasport{id}")]
-        public async Task<IActionResult> GetPatientFeedbackById(Guid id)
-        {
-            var feedback = await dbContext.PatientFeedbacks
-                .Where(f => f.feedbackId == id)
-                .FirstOrDefaultAsync();
-            if (feedback is null)
             {
-                return NotFound();
+            var result = _feedbacksService.GetAllPatientsFeedbacks();
+
+            return result == null ? NotFound("No feedbacks found.") : Ok(result);
             }
-            return Ok(feedback);
-        }
+
+
+
+        [HttpGet("patientId{id}")]
+        public async Task<IActionResult> GetPatientFeedbackById(Guid id)
+            {
+            var result = await _feedbacksService.GetFeedbacksByPatientId(id);
+            return result == null ? NotFound("No feedback found.") : Ok(result);
+            }
 
 
         [HttpGet("{id:guid}")]
         public IActionResult GetPatientFeedbackByUId(Guid id)
-        {
-            var feedback = dbContext.PatientFeedbacks.Find(id);
-            if (feedback is null)
             {
+            var feedback = _feedbacksService.GetFeedbackByUId(id);
+            if (feedback is null)
+                {
                 return NotFound();
-            }
+                }
             return Ok(feedback);
-        }
+            }
 
 
 
         [HttpPost]
-        public IActionResult AddPatientFeedback(NewPatientFeedbackDto newPatientFeedback)
-        {
-            var patientFeedbackEntity = new PatientFeedback()
+        public IActionResult AddPatientFeedback(NewPatientFeedbackDto newPatientFeedbackDto)
             {
-                patientId = newPatientFeedback.patientId,
-                appointmentId = newPatientFeedback.appointmentId,
-                overallStatus = newPatientFeedback.overallStatus,
-                comments = newPatientFeedback.comments,
-                createdOn = newPatientFeedback.createdOn,
-                newSymptoms = newPatientFeedback.newSymptoms,
-                bodyPart = newPatientFeedback.bodyPart
-                };
-            dbContext.PatientFeedbacks.Add(patientFeedbackEntity);
-            dbContext.SaveChanges();
-            _treatmentService.UpdateTreatmentScore(newPatientFeedback.overallStatus, newPatientFeedback.appointmentId);
-            if (newPatientFeedback.overallStatus <= TreatmentsConst.TREATMENTS_MIN_SCORE)
-                _recommendationService.GenerateRecommendationAfterFeedback(patientFeedbackEntity);
-            return Ok(patientFeedbackEntity);
-        }
+            var newFeedback = _feedbacksService.AddPatientFeedback(newPatientFeedbackDto);
+            return Ok(newFeedback);
+            }
+        
 
         [HttpPut]
         [Route("{id:guid}")]
         public IActionResult UpdatePatientFeedback(Guid id, UpdatePatientFeedbackDto updatePatientFeedbackDto)
-        {
-            var feedback = dbContext.PatientFeedbacks.Find(id);
-            if (feedback is null)
             {
-                return NotFound();
+            var updated = _feedbacksService.UpdatePatientFeedback(id, updatePatientFeedbackDto);
+            return updated == null ? NotFound("No feedback found.") : Ok(updated);
             }
-            feedback.patientId = updatePatientFeedbackDto.patientId;
-            feedback.appointmentId = updatePatientFeedbackDto.appointmentId;
-            feedback.overallStatus = updatePatientFeedbackDto.overallStatus;
-            feedback.comments = updatePatientFeedbackDto.comments;
-            feedback.createdOn = updatePatientFeedbackDto.createdOn;
-            feedback.newSymptoms = updatePatientFeedbackDto.newSymptoms;
-            feedback.bodyPart = updatePatientFeedbackDto.bodyPart;
-
-            dbContext.SaveChanges();
-            return Ok(feedback);
-        }
 
 
         [HttpDelete]
         [Route("{id:guid}")]
         public IActionResult DeletePatientFeedback(Guid id)
-        {
-            var feedback = dbContext.PatientFeedbacks.Find(id);
-            if(feedback is null)
             {
-                return NotFound();
+            var deleted = _feedbacksService.DeletePatientFeedback(id);
+            return deleted == null ? NotFound("No feedback found.") : Ok(deleted);
+
             }
-            dbContext.PatientFeedbacks.Remove(feedback);
-            dbContext.SaveChanges();
-            return Ok("Patient deleted");
+        
         }
     }
-}
---
+++ Services/PatientsFeedbacksService.cs
@@ -0,0 +1,93 @@
+﻿using AltermedManager.Data;
+using AltermedManager.Models.Dtos;
+using AltermedManager.Models.Entities;
+using AltermedManager.Models.Enums;
+using AltermedManager.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AltermedManager.Controllers
+    {
+    
+    public class PatientsFeedbacksService
+        {
+        private readonly ApplicationDbContext dbContext;
+        private readonly TreatmentService _treatmentService;
+        private readonly RecommendationService _recommendationService;
+        public PatientsFeedbacksService(ApplicationDbContext dbContext, TreatmentService treatmentService,
+            RecommendationService recommendationService)
+            {
+            this.dbContext = dbContext;
+            _treatmentService = treatmentService;
+            _recommendationService = recommendationService;
+            }
+
+        public List<PatientFeedback> GetAllPatientsFeedbacks()
+            {
+            return dbContext.PatientFeedbacks.ToList();
+            }
+
+        public async Task<List<PatientFeedback>> GetFeedbacksByPatientId(Guid patientID)
+            {
+            var feedbacks = await dbContext.PatientFeedbacks
+                .Where(f => f.patientId == patientID)
+                .ToListAsync();
+
+            return feedbacks;
+            }
+
+        public PatientFeedback? GetFeedbackByUId(Guid id)
+            {
+            return dbContext.PatientFeedbacks.Find(id);
+            
+            }
+
+              
+        public PatientFeedback AddPatientFeedback(NewPatientFeedbackDto newPatientFeedback)
+            {
+            var patientFeedbackEntity = new PatientFeedback()
+                {
+                patientId = newPatientFeedback.patientId,
+                appointmentId = newPatientFeedback.appointmentId,
+                overallStatus = newPatientFeedback.overallStatus,
+                comments = newPatientFeedback.comments,
+                createdOn = newPatientFeedback.createdOn,
+                newSymptoms = newPatientFeedback.newSymptoms,
+                bodyPart = newPatientFeedback.bodyPart
+                };
+            dbContext.PatientFeedbacks.Add(patientFeedbackEntity);
+            dbContext.SaveChanges();
+            _treatmentService.UpdateTreatmentScore(newPatientFeedback.overallStatus, newPatientFeedback.appointmentId);
+            if (newPatientFeedback.overallStatus <= TreatmentsConst.TREATMENTS_MIN_SCORE)
+                _recommendationService.GenerateRecommendationAfterFeedback(patientFeedbackEntity);
+            return patientFeedbackEntity;
+            }
+
+        public PatientFeedback? UpdatePatientFeedback(Guid id, UpdatePatientFeedbackDto updatePatientFeedbackDto)
+            {
+            var feedback = dbContext.PatientFeedbacks.Find(id);
+            if (feedback is null) return null;
+                
+            feedback.patientId = updatePatientFeedbackDto.patientId;
+            feedback.appointmentId = updatePatientFeedbackDto.appointmentId;
+            feedback.overallStatus = updatePatientFeedbackDto.overallStatus;
+            feedback.comments = updatePatientFeedbackDto.comments;
+            feedback.createdOn = updatePatientFeedbackDto.createdOn;
+            feedback.newSymptoms = updatePatientFeedbackDto.newSymptoms;
+            feedback.bodyPart = updatePatientFeedbackDto.bodyPart;
+
+            dbContext.SaveChanges();
+            return feedback;
+            }
+
+
+        public PatientFeedback? DeletePatientFeedback(Guid id)
+            {
+            var feedback = dbContext.PatientFeedbacks.Find(id);
+            if (feedback is null) return null;
+            dbContext.PatientFeedbacks.Remove(feedback);
+            dbContext.SaveChanges();
+            return feedback; //return deleted feedback
+            }
+        }
+    }
