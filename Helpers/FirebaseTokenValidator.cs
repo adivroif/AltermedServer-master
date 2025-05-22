f884f6ebdcd33522e2b890a579dcb1286ffc1101
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-05-22 20:50:11 +0300

    Add helper for handle firebase token

---
+++ Helpers/FirebaseTokenValidator.cs
@@ -0,0 +1,21 @@
+﻿using FirebaseAdmin.Auth;
+
+namespace AltermedManager.Helpers
+    {
+    public class FirebaseTokenValidator
+        {
+        public static async Task<string?> VerifyTokenAsync(string idToken)
+            {
+            try
+                {
+                var decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+                return decodedToken.Uid;
+                }
+            catch (Exception ex)
+                {
+                Console.WriteLine("Firebase token verification failed: " + ex.Message);
+                return null;
+                }
+            }
+        }
+    }
