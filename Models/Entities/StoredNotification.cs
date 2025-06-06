3141345dcce01d65452947278079573bd3dfcb73
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-06-06 23:21:52 +0300

    add StoredNotification entity

---
+++ Models/Entities/StoredNotification.cs
@@ -0,0 +1,21 @@
+﻿using AltermedManager.Models.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AltermedManager.Models.Entities
+    {
+    [Table("StoredNotifications")]
+    public class StoredNotification
+        {
+        [Key]
+        public int id { get; set; }
+        [ForeignKey("userId")]
+        public Guid userId { get; set; } // Doctor or patient
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string body { get; set; }
+        public bool isRead { get; set; }
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+        }
+    }
