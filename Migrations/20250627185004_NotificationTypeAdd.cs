1e18b4072741cacd18e42e34d7a9150a5af2359e
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-06-27 23:33:45 +0300

    new migrations, add type field to StoredNotification

---
+++ Migrations/20250627185004_NotificationTypeAdd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltermedManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationTypeAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "type",
+                table: "StoredNotifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "StoredNotifications");
+        }
+    }
+}
