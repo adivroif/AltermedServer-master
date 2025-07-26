34b6c942ab914be6e37661eeefbbe807f3ba10a4
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-07-26 22:15:32 +0300

    add patient from health provider model

---
+++ HealthProviderPatient.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthDummyProvider
+{
+    public class HealthProviderPatient 
+    {
+        
+        public required string patientID { get; set; }
+        public required string patientName { get; set; }
+        public required string patientSurname { get; set; }
+        public string? patientEmail { get; set; }
+        public string? patientPhone { get; set; }
+        public Address patientAddress { get; set; }
+        public required string healthProvider { get; set; }
+        public char gender { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        }
+}
