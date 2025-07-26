675f6129fe6864f161b7245ef764ff52aa0a5acb
Veronika Merkulova <Veronika.Merkulova@s.afeka.ac.il>
2025-07-26 22:15:07 +0300

    add Address from hp

---
+++ Address.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthDummyProvider
+    {
+    public class Address
+        {
+        public string city { get; set; }
+        public string street { get; set; }
+        public string postalCode { get; set; }
+        public string houseNumber { get; set; }
+        
+        }
+    }
